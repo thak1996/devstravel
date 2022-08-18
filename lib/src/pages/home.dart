cf28364edbd7ebf611dd42fca781f95297cd09f6
Franklyn Viana <franklyn_vs_@hotmail.com>
2022-08-18 19:37:49 -0300

    feature/home

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('testando 1,2,3'),
+      ),
+    );
+  }
+}
