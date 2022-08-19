584a34b3d73a07cc1a87c2f766e56936a654db0a
Franklyn Viana <franklyn_vs_@hotmail.com>
2022-08-19 10:25:41 -0300

    feature/preload/infos

--- lib/src/pages/preload.dart
+++ lib/src/pages/preload.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 
 class PreloadPage extends StatefulWidget {
+  const PreloadPage({super.key});
+
   @override
   _PreloadPage createState() => _PreloadPage();
 }
 
 class _PreloadPage extends State<PreloadPage> {
-  bool loading = false;
+  bool loading = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('lib/assets/flutter1_devstravel_logo.png',
-              width: 200,
-            )
-          ],
-        ),
-      )
-    );
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'lib/assets/flutter1_devstravel_logo.png',
+            width: 200,
+          ),
+          loading
+              ? Container(
+                  margin: const EdgeInsets.all(20),
+                  child: const Text(
+                    'Carregando informações...',
+                    style: TextStyle(fontSize: 16),
+                  ))
+              : Container(),
+          loading
+              ? const CircularProgressIndicator(
+                  strokeWidth: 3,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                )
+              : Container(),
+          !loading
+              ? Container(
+                  margin: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    child: const Text('Tentar Novamente'),
+                    onPressed: () {},
+                  ))
+              : Container(),
+        ],
+      ),
+    ));
   }
 }
